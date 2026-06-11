12e887359a320c981f5c5f7bfe0476609c4b70bd
Alejandro <AlexDoGa.work@gmail.com>
2026-06-11 23:21:25 +0100

    Configurar plugin SQLite local

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,12 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+
+        // SQLite
+        .plugin(
+            tauri_plugin_sql::Builder::default().build()
+        )
+
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 
   server: {
     port: 1420,
     strictPort: true,
+    watch: {
+      ignored: [
+        "**/src-tauri/target/**",
+      ],
+    },
   },
 });
